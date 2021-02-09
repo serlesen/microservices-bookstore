42015b7ee57405226105fa9c81778924531a1204
Sergio Lema <serlesen@gmail.com>
2021-02-09 18:16:42 +0100

    Added a service to manage the distributed configuration

---
+++ service-config/src/test/java/com/sergio/bookstore/serviceconfig/DistributedConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.bookstore.serviceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
